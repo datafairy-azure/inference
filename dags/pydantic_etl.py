2c6fa67f33fb4fe8f5f14ea6e58b05d9b5d10b9f
Martha <marthalasia@gmail.com>
2023-12-01 10:07:05 +0100

    Update pydantic_etl.py
    non-task functions have to be declared before dag definition

--- dags/pydantic_etl.py
+++ dags/pydantic_etl.py
@@ -7,15 +7,6 @@ from airflow.providers.microsoft.azure.hooks.wasb import WasbHook
 from airflow.decorators import dag, task
 
 
-@dag(
-    schedule="@daily",
-    start_date=pm.datetime(2023, 1, 1),
-    catchup=False,
-    default_args={
-        "retries": 2,
-    },
-    tags=["example"],
-)
 class InputData(pydantic.BaseModel):
     columns: List[int]
     index: List[int]
@@ -27,11 +18,11 @@ class InputData(pydantic.BaseModel):
         """Validator to check whether columns are valid"""
         if len(field_value) != 23:
             raise ValueError("Columns should be of length 23")
-
+        
         for x in field_value:
-            if x not in range(0, 23):
+            if x not in range(0,23):
                 raise ValueError("Columns should be in range 0-22")
-
+            
         return field_value
 
 
@@ -46,15 +37,21 @@ def clean_request(request: Request) -> dict:
     cleaned_dict = {}
     cleaned_dict["columns"] = request.input_data.columns
     cleaned_dict["index"] = request.input_data.index
-    cleaned_dict["data"] = [
-        max(item, 0) for items in request.input_data.data for item in items
-    ]
+    cleaned_dict["data"] = [max(item, 0) for items in request.input_data.data for item in items]
     return cleaned_dict
 
-
+@dag(
+    schedule="@daily",
+    start_date=pm.datetime(2023, 1, 1),
+    catchup=False,
+    default_args={
+        "retries": 2, 
+    },
+    tags=["example"],
+) 
 def etl_pipeline():
     """
-    ### ETL pipeline
+    ### ETL pipeline 
     """
 
     @task()
@@ -63,7 +60,7 @@ def etl_pipeline():
         Loads json requests using the Request model and returns a list of Request objects.
         """
 
-        sample_request_1 = """{
+        sample_request_1 = '''{
             "input_data": {
                 "columns": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22],
                 "index": [0, 1],
@@ -72,9 +69,9 @@ def etl_pipeline():
                     [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 10, 9, 8]
                     ]
             }
-        }"""
+        }'''
 
-        sample_request_2 = """{
+        sample_request_2 = '''{
             "input_data": {
                 "columns": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22],
                 "index": [0, 1],
@@ -83,7 +80,7 @@ def etl_pipeline():
                     [5, 4, 3, 2, 1, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 9, 8, 7, 6, 5, 4, 3]
                     ]
             }
-        }"""
+        }'''
 
         ordered_data = []
 
@@ -115,15 +112,12 @@ def etl_pipeline():
         request_locations = []
         blob_connection = WasbHook(wasb_conn_id="connection_id_blob")
         for item in cleaned_requests:
-            blob_connection.load_string(
-                item, "azureml", f"request_sample_{pm.now().timestamp()}.json"
-            )
+            blob_connection.load_string(item, 'azureml', f"request_sample_{pm.now().timestamp()}.json")
             request_locations.append(f"request_sample_{pm.now().timestamp()}.json")
         return request_locations
-
+    
     raw_data = load_data()
     cleaned_data = clean_data(raw_data)
     prepare_requests(cleaned_data)
-
 
 etl_pipeline()
